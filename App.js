eef638b4dfcf7f708b9d98abd3b3555720358970
maruhachi <konchan1129@gmail.com>
2019-06-22 22:53:55 +0900

    add offline mode alias to package.json.

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Awaken the Power!</Text>
+      <Text>REAH and Ruby from LoveLive! Sunshine!!</Text>
     </View>
   );
 }
