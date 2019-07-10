096b11cfe84203b45b7080a0ed5709676da859a6
maruhachi <konchan1129@gmail.com>
2019-07-10 09:05:15 +0900

    進捗のコミット(component, enable eslint)

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import BodyText from './src/elements/BodyText';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Awaken the Power!</Text>
-      <Text>REAH and Ruby from LoveLive! Sunshine!!</Text>
+      <View style={styles.appbar}>
+        <View>
+          <Text>MemoApp</Text>
+        </View>
+      </View>
+
+      <View style={styles.memoList}>
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイテム</Text>
+          <Text style={styles.memoDate}>2019/07/10</Text>
+        </View>
+      </View>
+
+      <View style={styles.memoList}>
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>小原鞠莉</Text>
+          <Text style={styles.memoDate}>2019/06/13</Text>
+        </View>
+      </View>
+
+      <View style={styles.memoList}>
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>津島善子</Text>
+          <Text style={styles.memoDate}>2019/07/15</Text>
+        </View>
+      </View>
+
+      <View>
+        <Text>+</Text>
+      </View>
     </View>
   );
 }
