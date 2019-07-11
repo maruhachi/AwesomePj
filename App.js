f061cf0f90647845b642a031ee2a1ee008faf398
maruhachi <konchan1129@gmail.com>
2019-07-12 01:01:21 +0900

    進捗状況のコミット(2つめの画面)

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import AppBar from './src/components/AppBar';
-import MemoListScreen from './src/screens/MemoListScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <AppBar />
-      <MemoListScreen />
+      <MemoDetailScreen />
     </View>
   );
 }
--- src/elements/CircleButton.js
+++ src/elements/CircleButton.js
@@ -3,16 +3,25 @@ import { StyleSheet, View, Text } from 'react-native';
 
 class CircleButton extends React.Component {
   render() {
+    const { style, color } = this.props;
+
+    let bgColor = '#E31676';
+    let textColor = '#fff';
+
+    if (color === 'white') {
+      bgColor = '#fff';
+      textColor = '#E31676';
+    }
     return (
-      <View style={styles.memoAddButton}>
-        <Text style={styles.memoAddButtonTitle}>{this.props.children}</Text>
+      <View style={[styles.circleButton, style, { backgroundColor: bgColor }]}>
+        <Text style={[styles.circleButtonTitle, { color: textColor }]}>{this.props.children}</Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  memoAddButton: {
+  circleButton: {
     position: 'absolute',
     bottom: 32,
     right: 32,
@@ -27,7 +36,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.2,
     shadowRadius: 3,
   },
-  memoAddButtonTitle: {
+  circleButtonTitle: {
     fontSize: 32,
     lineHeight: 32,
     color: '#fff',
