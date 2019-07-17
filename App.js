c51b9ebb7ff9f58e5367b4cc26658ebfadc540ac
maruhachi <konchan1129@gmail.com>
2019-07-17 18:54:09 +0900

    ログイン画面の基盤を作成

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import AppBar from './src/components/AppBar';
-import MemoEditScreen from './src/screens/MemoEditScreen';
+import LoginScreen from './src/screens/LoginScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <AppBar />
-      <MemoEditScreen />
+      <LoginScreen />
     </View>
   );
 }
---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  StyleSheet, View, Text, TextInput, TouchableHighlight,
+} from 'react-native';
+
+import CircleButton from '../elements/CircleButton';
+
+class LoginScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>ログイン</Text>
+        <TextInput style={styles.input} value="Email address" />
+        <TextInput style={styles.input} value="password" />
+        <TouchableHighlight style={styles.button} underlayColor="#C70f66" onPress={() => {}}>
+          <Text>送信</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    padding: 24,
+    backgroundColor: '#fff',
+  },
+  input: {
+    backgroundColor: '#eee',
+    height: 48,
+    marginBottom: 16,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    padding: 8,
+  },
+  title: {
+    fontSize: 28,
+    alignSelf: 'center',
+    marginBottom: 24,
+  },
+  button: {
+    backgroundColor: '#E31676',
+    height: 40,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '70%',
+    alignSelf: 'center',
+  },
+});
+
+export default LoginScreen;
