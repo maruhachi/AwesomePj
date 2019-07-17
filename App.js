0dca33c55896b916b1b66a8a1cd6497e61935fd0
maruhachi <konchan1129@gmail.com>
2019-07-17 18:32:53 +0900

    編集画面の作成

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import AppBar from './src/components/AppBar';
-import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <AppBar />
-      <MemoDetailScreen />
+      <MemoEditScreen />
     </View>
   );
 }
---
+++ src/screens/MemoEditScreen.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  StyleSheet, View, Text, TextInput,
+} from 'react-native';
+
+import CircleButton from '../elements/CircleButton';
+
+class MemoEditScreen extends React.Component {
+  render() {
+    return (
+      <View style={style.container}>
+        <TextInput style={style.memoEditInput} multiline="true" value="抹茶プリン" />
+        <CircleButton name="check" />
+      </View>
+    );
+  }
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+  },
+  memoEditInput: {
+    backgroundColor: '#fff',
+    flex: 1,
+    paddingTop: 32,
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingBottom: 16,
+    fontSize: 16,
+  },
+});
+
+export default MemoEditScreen;
