dc75017ba327a29c787a9b4211faef924ac0bc13
ykonno <konchan1129@gmail.com>
2019-07-18 17:31:36 +0900

    React-Native-Webで起動したかった

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const appDirectory = path.resolve(__dirname, './');
+
+const babelLoaderConfiguration = {
+    test: /\.js$/,
+    include: [
+        path.resolve(appDirectory, 'index.web.js'),
+        path.resolve(appDirectory, 'src')
+    ],
+    use: {
+        loader: 'babel-loader',
+        options: {
+            cacheDirectory: true,
+            plugins: ['react-native-web'],
+            presets: [
+                '@babel/preset-env',
+                'react-native']
+        }
+    }
+}
+
+module.exports = {
+    entry: path.resolve(appDirectory, 'index.web.js'),
+    output: {
+        filename: 'bundle.web.js',
+        path: path.resolve(appDirectory, 'dist')
+    },
+    module: {
+        rules: [
+            babelLoaderConfiguration
+        ]
+    },
+    resolve: {
+        alias: {
+            'react-native$': 'react-native-web'
+        }
+    }
+}
