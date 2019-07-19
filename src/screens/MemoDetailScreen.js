b8fde8dba5c661d0b440ad5beaf9709ecad59ef3
ykonno <konchan1129@gmail.com>
2019-07-19 11:31:25 +0900

    React-Native-Webでの動作確認が可能になった

--- src/screens/MemoDetailScreen.js
+++ src/screens/MemoDetailScreen.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
-import ThreeAxisSensor from 'expo-sensors/build/ThreeAxisSensor';
 import CircleButton from '../elements/CircleButton';
 
 class MemoDetailScreen extends React.Component {
